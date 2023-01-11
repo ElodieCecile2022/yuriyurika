875e3b3f90e4581ab68f275021ac12b89cdd0789
Elodie2023 <110039272+ElodieCecile2022@users.noreply.github.com>
2023-01-11 11:03:50 +0100

    Initial Commit

---
+++ yuriyurika/yuriyurikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuriyurikaApp.swift
+//  yuriyurika
+//
+//  Created by Elodie Iparraguirre on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct yuriyurikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
